0001948ade3f6fc337102664a4f7bca75169a996
Alexandre Corazza Marcolino <alexandre_corazza@yahoo.com.br>
2024-02-29 18:42:59 -0300

    validate if player X or player O wins

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+const playerXWins = (board) => {
+  if ((board[0] === 'X' && board[1] === 'X' && board[2] === 'X') ||
+         (board[3] === 'X' && board[4] === 'X' && board[5] === 'X') ||
+         (board[6] === 'X' && board[7] === 'X' && board[8] === 'X') ||
+         (board[0] === 'X' && board[3] === 'X' && board[6] === 'X') ||
+         (board[1] === 'X' && board[4] === 'X' && board[7] === 'X') ||
+         (board[2] === 'X' && board[5] === 'X' && board[8] === 'X') ||
+         (board[0] === 'X' && board[4] === 'X' && board[8] === 'X') ||
+         (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+const playerOWins = (board) => {
+  if ((board[0] === 'O' && board[1] === 'O' && board[2] === 'O') ||
+         (board[3] === 'O' && board[4] === 'O' && board[5] === 'O') ||
+         (board[6] === 'O' && board[7] === 'O' && board[8] === 'O') ||
+         (board[0] === 'O' && board[3] === 'O' && board[6] === 'O') ||
+         (board[1] === 'O' && board[4] === 'O' && board[7] === 'O') ||
+         (board[2] === 'O' && board[5] === 'O' && board[8] === 'O') ||
+         (board[0] === 'O' && board[4] === 'O' && board[8] === 'O') ||
+         (board[2] === 'O' && board[4] === 'O' && board[6] === 'O')) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
 const ticTacToe = (board) => {
   if (!Array.isArray(board) || board.length != 9) {
     return false;
@@ -10,4 +40,6 @@ const ticTacToe = (board) => {
   
 module.exports = {
   ticTacToe,
+  playerXWins,
+  playerOWins
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const { ticTacToe } = require("../src/index");
+const { ticTacToe, playerXWins, playerOWins } = require("../src/index");
 
 test("should return false if the board is an array with a length other than 9", () => {
     expect(ticTacToe(['', '', ''])).toBe(false);
@@ -6,4 +6,12 @@ test("should return false if the board is an array with a length other than 9", 
 
 test("should return 'Player X wins' if the board is an array filled like ['X', 'X', 'X', '', '', '', '', '', '']", () => {
     expect(ticTacToe(['X', 'X', 'X', '', '', '', '', '', ''])).toBe('Player X wins');
+});
+
+test("should return true if function playerXWins receive a array filled like ['X', 'X', 'X', '', '', '', '', '', '']", () => {
+    expect(playerXWins(['X', 'X', 'X', '', '', '', '', '', ''])).toBe(true);
+});
+
+test("should return true if function playerOWins receive a array filled like ['X', 'X', 'X', '', '', '', '', '', '']", () => {
+    expect(playerOWins(['O', 'O', 'O', '', '', '', '', '', ''])).toBe(true);
 });
