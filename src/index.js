b04899d3f16536848b254f9ede9e6c7587c9eb62
Alexandre Corazza Marcolino <alexandre_corazza@yahoo.com.br>
2024-02-29 18:37:45 -0300

    player X wins with a horizontal line

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const ticTacToe = (board) => {
-  return !board;
+  if (!Array.isArray(board) || board.length != 9) {
+    return false;
+  } else {
+    if (board[0] === 'X' && board[1] === 'X' && board[2] === 'X') {
+      return 'Player X wins'
+    }
+  }
 };
   
 module.exports = {
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,3 +3,7 @@ const { ticTacToe } = require("../src/index");
 test("should return false if the board is an array with a length other than 9", () => {
     expect(ticTacToe(['', '', ''])).toBe(false);
 });
+
+test("should return 'Player X wins' if the board is an array filled like ['X', 'X', 'X', '', '', '', '', '', '']", () => {
+    expect(ticTacToe(['X', 'X', 'X', '', '', '', '', '', ''])).toBe('Player X wins');
+});
